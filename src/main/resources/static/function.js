249f2b3535e76fd53ba4fbd17766c33948167a6f
leonwvink <38214418+leonwvink@users.noreply.github.com>
2018-05-15 12:08:23 +0200

    Adding airport finally

--- src/main/resources/static/function.js
+++ src/main/resources/static/function.js
@@ -1,55 +1,13 @@
-function postData() {
-         console.log("posting data...");
+function newAirport(){
+var title = $('#title').val();
+    var newOrder = {
+        name: title
+    };
+    console.log(newOrder)
 
-
-        // Create JS object with data.
-        var newAirport = {
-            name: $("#inputName").val(),
-        };
-
-        $.post("http://localhost:8080/api/airport/create", newAirport, function(result) {
-            getData();
-        });
-
- }
-
-
-function getData() {
-     // Get the data from endpoint.
-    $.ajax({
-        url:"http://localhost:8080/api/airport/all",
-        type:"get",
-        success: function(data) {
-            // On successful get, reload the datatable with new data.
-            $('#table').DataTable().clear();
-            $('#table').DataTable().rows.add(data);
-            $('#table').DataTable().columns.adjust().draw();
-        }
-    });
+    $.post("http://localhost:8080/api/airport/create", newOrder, function(data) {});
 }
 
-$(document).ready(function () {
-    // Modal submit.
-    $("#newAirportForm").on('submit', function(e) {
-        console.log("Submitted new Dish form");
 
-        // Post the data from the modal.
-        postData();
-
-        // Reset modal to hide and no values.
-        $('#newAirportModal').modal('hide');
-
-    });
-
- // Load DataTable with data format.
-    $('#table').DataTable({
-        "order": [[ 2, "desc" ]],
-        columns: [
-            { "data": "id" },
-            { "data": "name" }
-        ]
-    });
-
-    // Load first data.
-    getData();
+$(document).ready( function(){
 });
